f89575b6428f97d908869a09b33c1a876214608b
Triniti <dungey@marshall.edu>
2025-05-05 05:23:54 -0400

    new map

--- src/App.tsx
+++ src/App.tsx
@@ -342,12 +342,10 @@ function App() {
           <div className="max-w-4xl mx-auto text-center">
             <blockquote className="text-3xl font-light leading-relaxed mb-8">
               "Insert map here"
-              <a href="https://mapmyvisitors.com/web/1by7l"  title="Visit tracker"><img src="https://mapmyvisitors.com/map.png?d=BkyeK9AhJPLWBwhLczb3lZI3hIMIVxz3xtvjM0-S5Xk&cl=ffffff" /></a>
+              <a href='https://mapmyvisitors.com/web/1by7l'  title='Visit tracker'><img src='https://mapmyvisitors.com/map.png?cl=080808&w=753&t=tt&d=BkyeK9AhJPLWBwhLczb3lZI3hIMIVxz3xtvjM0-S5Xk&co=ffffff&ct=808080'/></a>
             </blockquote>
-            <a href="https://mapmyvisitors.com/web/1by7l"  title="Visit tracker"><img src="https://mapmyvisitors.com/map.png?d=BkyeK9AhJPLWBwhLczb3lZI3hIMIVxz3xtvjM0-S5Xk&cl=ffffff" /></a>
             <p className="text-xl font-semibold">Sara Mumolo</p>
             <p className="text-lg">Director of Online Outreach Programs</p>
-            <a href="https://mapmyvisitors.com/web/1by7l"  title="Visit tracker"><img src="https://mapmyvisitors.com/map.png?d=BkyeK9AhJPLWBwhLczb3lZI3hIMIVxz3xtvjM0-S5Xk&cl=ffffff" /></a>
           </div>
         </section>
 
